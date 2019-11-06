dbf74eec9d49950e8d54e4befba4d3de45b6ebb0
Finny Abraham <kozakk@gmail.com>
2019-11-06 14:46:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays () { return "Happy holidays!"}
 function happyHolidaysTo (name) { return `Happy holidays, ${name}!`}
 function happyHolidayTo (holiday, name) {
   return `Happy ${holiday}, ${name}!`}
-function holidayCountdown (holiday, days) {
+function holidayCountdown (days, holiday) {
   `It\'s ${days} days until ${holiday}!`
 }
